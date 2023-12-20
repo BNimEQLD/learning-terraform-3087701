f8e3cc9c41575898371165f69d4e499fe5d866fe
BNimEQLD <152142197+BNimEQLD@users.noreply.github.com>
2023-12-20 11:45:34 +1000

    syntax fixed

--- main.tf
+++ main.tf
@@ -59,7 +59,6 @@ module "alb" {
   subnets             = module.blog_vpc.public_subnets
   security_groups     = [module.blog_sg.security_group_id]
 
-
   target_groups = [
     {
       name_prefix      = "blog-"
